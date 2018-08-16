daea5e80a282deb5575dc8b124b54c6663558aa1
Moritz Rinow <mrinow.dev@gmail.com>
2018-08-16 22:49:52 +0200

    Added IStreamSerializable

---
+++ ezNet/Serialization/Public/IStreamSerializable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Common/Public/Defines.h"
+#include "Stream/Public/IStream.h"
+
+EZNET_BEGIN
+
+class IStreamSerializable;
+
+/*
+* Interface to work with objects, that can be serialized to streams.
+*/
+class IStreamSerializable {
+
+public:
+
+  /*
+  * Virtual deconstructor.
+  */
+  virtual ~IStreamSerializable() {}
+
+  /*
+  * This should serialize the current instance behind the interface to a stream.
+  */
+  virtual void Serialize(IStream* stream) = 0;
+};
+
+EZNET_END
